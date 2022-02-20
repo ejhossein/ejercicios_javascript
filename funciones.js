63e12725ec9a28dd54b39dca53f9b6e9b16af09f
joelhossein <joelhossein@protonmail.com>
2022-02-20 13:39:24 -0300

    se agrego funciones.js

---
+++ funciones.js
@@ -0,0 +1,4 @@
+// Armar funcion sumar, restar,
+
+// Armar una función que se llame palabraLarga que reciba un string como parametro y devuelva true si tiene más de 20
+// caracteres, de lo contrario devuelve false
